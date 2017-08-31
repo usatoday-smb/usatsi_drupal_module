8b945a897695d5b48d62f74c0d8e99c49c7b0ddd
Thomas Rivera <thomasrivera@Thomass-MBP.home>
2017-08-31 19:05:35 -0400

    updates to preview overlay

--- media_usatsi/js/media_usatsi.js
+++ media_usatsi/js/media_usatsi.js
@@ -1,12 +1,36 @@
 (function ($) {
   'use strict';
+
+
   Drupal.behaviors.media_usatsi = {
     attach: function (context, settings) {
+
+
+        $('body').delegate( "li  img", "mouseenter", function(e) {
+            e.stopPropagation();
+            $(this).parent().css('top','-25%').css('left', '-25%');
+            $(this).closest('li').find('div.on-hover-content').fadeIn();
+        });
+
+       // $('body').delegate( "p", "mouseleave", function(e) {
+           // e.stopPropagation();
+           // $(this).closest('li').find('div.on-hover-content').fadeOut();
+       // });
+
+
+        $('body').delegate('#media-usatsi-external .on-hover-content', 'mouseleave', function(e) {
+            e.stopPropagation();
+            //$(this).clearQueue();
+            $(this).fadeOut();
+        });
+
+      //Add Class to Tab.
+      $('.tabs.primary a[href="/file/add/usatsi"]').attr('id','media-usatsi-tab');
 
       // Hide submit button.
       $('form#media-usatsi-external .form-actions .form-submit').hide();
 
-      $('img.usatsi').once().bind('click', function (e) {
+      $('img.usatsi').bind('click', function (e) {
 
         //Get Image Data from data attribute
         var usatsi_url = $(this).attr('data-image'),
@@ -25,6 +49,16 @@
         // Autosubmit form.
         $('#media-usatsi-external').submit();
       });
+
+
+
+            /*console('gekki');
+            //$(this).next("div.on-hover-content").fadeToggle( "slow", "linear" );
+        })
+            .bind('mouseleave',function(){
+
+                // $(this).next("div.on-hover-content").fadeToggle( "slow", "linear" );
+            }); */
     }
   };
 }(jQuery));
--- media_usatsi/templates/media-usatsi-list.tpl.php
+++ media_usatsi/templates/media-usatsi-list.tpl.php
@@ -11,33 +11,48 @@
 ?>
 
 
-<div  class="media-usatsi-pagination"></div>
+<div class="media-usatsi-pagination"></div>
 <div class="media-list-thumbnails">
   <?php if (is_array($images)): ?>
     <?php foreach ($images as $key => $img): ?>
-      <li>
-        <div class="media-item">
-          <div class="media-thumbnail">
-            <img class="usatsi"
-                 data-image="<?php print $img['download']; ?>"
-                 src="<?php print $img['thumb']; ?>"
-                 data-headline="<?php print $img['title']; ?>"
-                 data-caption="<?php print $img['caption']; ?>"
-                 data-credit="<?php print $img['credit']; ?>" />
-            <div class="label-wrapper">
-              <?php print $img['title']; ?>
-            </div>
-            <div class="label-wrapper">
-              <?php print $img['people'] ?>
-            </div>
-            <div class="label-wrapper">
-                <?php print $img['date_created'] ?>
-            </div>
-          </div>
-        </div>
-      </li>
+        <?php if (!empty($img['thumb'])):  ?>
+            <li>
+                <div class="media-item">
+                  <div class="media-thumbnail">
+                    <img class="usatsi"
+                         data-image="<?php print $img['download']; ?>"
+                         src="<?php print $img['thumb']; ?>"
+                         data-headline="<?php print $img['title']; ?>"
+                         data-caption="<?php print $img['caption']; ?>"
+                         data-credit="<?php print $img['credit']; ?>" />
+                    <div class="label-wrapper">
+                      <?php print $img['title']; ?>
+                    </div>
+                    <div class="label-wrapper">
+                      <?php print $img['people'] ?>
+                    </div>
+                    <div class="label-wrapper">
+                        <?php print $img['date_created'] ?>
+                    </div>
+                  </div>
+                </div>
+                <div class="on-hover-content">
+                    <img src="<?php print $img['previewUrl']; ?>">
+                    <div class="label-wrapper label-title">
+                      <?php print $img['title'] ?>
+                    </div>
+                    <div class="label-wrapper label-caption">
+                      <?php print $img['caption'] ?>
+                    </div>
+                    <div class="label-wrapper label-credit">
+                      <?php print $img['credit'] ?>
+                    </div>
+                </div>
+          </li>
+      <?php endif; ?>
     <?php endforeach; ?>
   <?php else: ?>
     <?php print $images; ?>
   <?php endif; ?>
 </div>
+<div class="media-usatsi-pagination-btm"></div>
